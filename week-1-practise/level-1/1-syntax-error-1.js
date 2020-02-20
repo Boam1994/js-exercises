7232d1f72273df8565f2703c4cfe10b8dc9d6762
Boam1994 <59532845+Boam1994@users.noreply.github.com>
2020-02-20 16:45:15 +0100

    Update syntax-error-1.js

--- week-1-practise/level-1/1-syntax-error-1.js
+++ week-1-practise/level-1/1-syntax-error-1.js
@@ -1,6 +1,6 @@
 // There is a syntax error in this code. Fix it.
 
-function addNumbers(a b c) {
+function addNumbers(a,b,c) {
     return a + b + c;
 }
 
@@ -12,3 +12,22 @@ console.log(result); // Expected result 8 - Run the program and make sure it wor
 // 1. How many parameters does the function take?
 // 2. What is the function name?
 // 3. Where is the function called? with what parameters?
+
+// 1. the function take 3 parameters.
+// 2. the funtion named "addNumbers".
+// 3. i don't understant the first part of the question. wiht parameters a , b and c.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
